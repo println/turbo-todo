e29c6cce2e1769122c13f59da744aca100ad74b4
FelipeSantos <live.proto@hotmail.com>
2023-01-26 11:23:36 -0300

    ajustando ci

--- src/app/modules/domain/domain.module.ts
+++ src/app/modules/domain/domain.module.ts
@@ -1,11 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { DomainRoutingModule } from './domain-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule, DomainRoutingModule],
+  imports: [DomainRoutingModule],
 })
 export class DomainModule {}
